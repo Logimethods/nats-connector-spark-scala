a06c5be4b6e72a9100909738b75e22eeba973202
Laurent Magnin <laurent.magnin@logimethods.com>
2016-08-25 11:04:13 -0400

    [TEST] instance of SparkToStandardNatsConnectorPoolScala

--- src/test/scala/com/logimethods/scala/connector/spark/to_nats/SparkToNatsConnectorPoolTest.scala
+++ src/test/scala/com/logimethods/scala/connector/spark/to_nats/SparkToNatsConnectorPoolTest.scala
@@ -3,6 +3,11 @@ package com.logimethods.scala.connector.spark.to_nats
 import collection.mutable.Stack
 import org.scalatest._
 
-class SparkToNatsConnectorPoolTest extends FlatSpec with Matchers {
+class SparkToNatsConnectorPoolTest extends FunSuite {
+  
+  test("SparkToNatsConnectorPool.newPool() should return an instance of SparkToStandardNatsConnectorPoolScala") {
+    val pool = SparkToNatsConnectorPool.newPool()
+    assert(pool.isInstanceOf[SparkToStandardNatsConnectorPoolScala])
+  }
   
 }
