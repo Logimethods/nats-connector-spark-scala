213f29b98fa6c6b37b086d06a71bdf706b0c2ec1
Laurent Magnin <laurent.magnin@logimethods.com>
2016-08-25 11:17:39 -0400

    [TEST] Connector should be a SparkToStandardNatsConnectorImpl

--- src/test/scala/com/logimethods/scala/connector/spark/to_nats/SparkToNatsConnectorPoolTest.scala
+++ src/test/scala/com/logimethods/scala/connector/spark/to_nats/SparkToNatsConnectorPoolTest.scala
@@ -2,6 +2,7 @@ package com.logimethods.scala.connector.spark.to_nats
 
 import collection.mutable.Stack
 import org.scalatest._
+import com.logimethods.connector.spark.to_nats._
 
 class SparkToNatsConnectorPoolTest extends FunSuite {
   
@@ -10,4 +11,9 @@ class SparkToNatsConnectorPoolTest extends FunSuite {
     assert(pool.isInstanceOf[SparkToStandardNatsConnectorPoolScala])
   }
   
+  test("Connector should be a SparkToStandardNatsConnectorImpl") {
+    val pool = SparkToNatsConnectorPool.newPool().withSubjects("Subject")
+    val connector = pool.getConnector()
+    assert(connector.isInstanceOf[SparkToStandardNatsConnectorImpl])
+  }  
 }
