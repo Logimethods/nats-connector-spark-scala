c88d90ffe732556acd4206d08fb91d167490d22c
Laurent Magnin <laurent.magnin@logimethods.com>
2016-11-10 10:31:31 -0500

    publishToNats(... java.util.function.Function...) REMOVED

--- src/main/scala/com/logimethods/scala/connector/spark/to_nats/SparkToNatsConnectorTrait.scala
+++ src/main/scala/com/logimethods/scala/connector/spark/to_nats/SparkToNatsConnectorTrait.scala
@@ -33,32 +33,11 @@ trait SparkToNatsConnectorPoolTrait[T] extends SparkToNatsConnectorPool[T] {
     }
   }
   
-  def publishToNats[V](stream: DStream[V], dataEncoder: java.util.function.Function[V, Array[Byte]]){
-    stream.foreachRDD { rdd =>
-      rdd.foreachPartition { partitionOfRecords =>
-			  val connector = getConnector();
-        partitionOfRecords.foreach(record => connector.publish(record, dataEncoder))
-        returnConnector(connector)  // return to the pool for future reuse
-      }
-    }
-  }
-  
   def publishToNats[V](stream: DStream[V], dataEncoder: scala.Function1[V, Array[Byte]]){
     stream.foreachRDD { rdd =>
       rdd.foreachPartition { partitionOfRecords =>
 			  val connector = getConnector();
         partitionOfRecords.foreach(record => connector.publish(record, dataEncoder))
-        returnConnector(connector)  // return to the pool for future reuse
-      }
-    }
-  }
-
-  def publishToNatsAsKeyValue[K, V](stream: DStream[Tuple2[K, V]], dataEncoder: java.util.function.Function[V, Array[Byte]]){
-    setStoredAsKeyValue(true);
-		stream.foreachRDD { rdd =>
-      rdd.foreachPartition { partitionOfRecords =>
-			  val connector = getConnector();
-        partitionOfRecords.foreach(record => connector.publishTuple(record, dataEncoder))
         returnConnector(connector)  // return to the pool for future reuse
       }
     }
